0b3818c26fe9861ebd46529ee9fff2bcf5104b32
dawsergiojerez@gmail.com <dawsergiojerez@gmail.com>
2018-12-28 22:49:08 +0000

    collecting properties from index.html on UI-class constructor

---
+++ src/app/UI.js
@@ -0,0 +1,21 @@
+export class UI {
+
+    constructor() {
+        this.location = document.getElementById('weather-location');
+        this.desc = document.getElementById('weather-description');
+        this.string = document.getElementById('weather-string');
+        this.humidity = document.getElementById('weather-humidity');
+        this.wind = document.getElementById('weather-wind');
+    }
+
+    //Fetch Data from the API in Weather-class in a object and fill the properties
+    //of this UI class 
+    render(weather) {
+
+    }
+
+
+
+
+
+}
